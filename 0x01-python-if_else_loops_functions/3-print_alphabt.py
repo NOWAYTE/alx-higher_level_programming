5fcbc383c767c1bbd6d49078eb41705949dcace8
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-09 03:33:52 +0300

    Fix code error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')):
-    alp = chr(i)
-
-    if i not in ['q', 'e']:
-        print('{}'.format(alp), end="")
+    if ((chr(i) != 'e') and (chr(i) != 'q')):
+        print(chr(i), end="")
