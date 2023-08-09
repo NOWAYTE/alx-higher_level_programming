c1481e41df23d47debb6f15c479e26885006f3f2
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-09 05:23:40 +0300

    Code update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,5 @@ def uppercase(s):
     for i in s:
         if 'a' <= s <= 'z':
             i = chr(ord(i) -32)  
-        print('{}'.format(s), end="")     
+        print('{}'.format(s), end="")
+    print()
