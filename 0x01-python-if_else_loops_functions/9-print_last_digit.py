f871aec26aaaa5c020ff0c665ddede76a3e5c1bf
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-09 05:36:41 +0300

    Cod update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    l_digit = abs(number) % 10
-    print('{}'.format(number), end="")
+    l_digit = number % 10
+    print('{}'.format(number), end=" ")
 
     return l_digit
