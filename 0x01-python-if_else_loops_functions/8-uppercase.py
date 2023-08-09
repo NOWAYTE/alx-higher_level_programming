daf13b94a3cbfe96888432553c1ef3a164bb6d82
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-09 05:29:32 +0300

    Fix code error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,6 @@
 def uppercase(s):
     for i in s:
         if 'a' <= s <= 'z':
-            i = (chr(i) - 32)
-    print('{}'.format(s), end="")
+            i = chr(ord(i) - 32)
+        print('{}'.format(s), end="")
+    print()
