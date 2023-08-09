53bd94664263b0596410f2324aa021f18d4c8c9b
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-09 05:55:48 +0300

    FizzBuzz challenge

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0  and i % 5:
-            print('{}'.format(i),"FizzBuzz", end=" ")
+        if i % 3 == 0 and i % 5:
+            print('{}'.format(i), "FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print('{}'.format(i),"Fizz", end=" ")
+            print('{}'.format(i), "Fizz", end=" ")
         elif i % 5 == 0:
-            print('{}'.format(i),"Buzz", end=" ")
+            print('{}'.format(i), "Buzz", end=" ")
         else:
             print(i, end=" ")
