0bc7955fee9dba463908cdc7cef581d58d43dc45
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 01:46:30 +0300

    Fix code error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matric_integer(matrix=[[]]):
     for i in matrix:
         for n in i:
-            if (len(matrix(i)) != 0 )
+            if (len(matrix(i)) != 0)
             print('{:d}'.format(n), end="")
         print()
