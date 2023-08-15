99246ee89dd3880577e7cf6ba8169f578767e190
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 01:40:14 +0300

    Code update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matric_integer(matrix=[[]]):
     for i in matrix:
-        print('{:d}'.format(*i), end="")
+        for n in i:
+            print('{:d}'.format(ij), end="")
