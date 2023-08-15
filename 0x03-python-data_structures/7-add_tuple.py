e5cf21af444591ce873204bb6a0083b6427fb02d
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 02:51:38 +0300

    Fix code error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x = tuple_a
-    y = tuple_b
+    x = tuple_a + (0, 0)
+    y = tuple_b + (0, 0)
 
     return (x[0] + y{0], x[1] + y[1])
