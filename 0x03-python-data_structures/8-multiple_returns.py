4673d12509895030e41914270a5abf83912f6ee2
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 03:10:01 +0300

    Fix TypeError

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (len(sentence) == ""):
-        sentence[0] = "None"
-        return (sentence)
+    if (sentence == ""):
+        return (0, None)
     print(len(sentence))
     return sentence[0]
