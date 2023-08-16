3edd43be255c923f0353e27d128d344490d307ef
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 03:00:27 +0300

    Concatenate tuple_a and tuple_b

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x = tuple_a
-    y = tuple_b
+    """A function that adds two tuples"""
+    x = tuple_a + (0, 0)
+    y = tuple_b + (0, 0)
+    """Concatenation ensures both the tuples have atleast two"""
 
     return (x[0] + y[0], x[1] + y[1])
