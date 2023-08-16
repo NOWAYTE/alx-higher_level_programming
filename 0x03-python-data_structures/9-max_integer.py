3d39513f285b268e607fc388aaa103c6c96552d8
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-16 03:24:09 +0300

    remove else statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return None
-    x = my_list[0]
+    x = 0
     for i in my_list:
         if i > x:
             x = i
-        else:
-            return i
     return i
