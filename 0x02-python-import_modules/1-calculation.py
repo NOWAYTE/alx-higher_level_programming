078a1d70db8ac29c7c59cf3580c175e963581e53
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 18:03:11 +0300

    Fix code error

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-from calculator import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-
-if __name__ == "___main__":
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-    print('{} - {} = {}'.format(a, b, sub(a, b)))
-    print('{} * {} = {}'.format(a, b, mul(a, b)))
-    print('{} / {} = {}'.fomrat(a, b, div(a, b)))
+sum_result = add(a, b)
+diff_result = subtract(a, b)
+prod_result = multiply(a, b)
+quot_result = divide(a, b)
+print("The sum of {} and {} is: {}".format(a, b, sum_result))
+print("The difference between {} and {} is: {}".format(a, b, diff_result))
+print("The product of {} and {} is: {}".format(a, b, prod_result))
+print("The quotient of {} divided by {} is: {}".format(a, b, quot_result))
