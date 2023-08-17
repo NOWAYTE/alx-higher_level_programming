869714ef59ad98834c11fbdb1a4a70d22f5d3e36
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 18:09:30 +0300

    Code update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,9 @@ sum_result = add(a, b)
 diff_result = subtract(a, b)
 prod_result = multiply(a, b)
 quot_result = divide(a, b)
-print("The sum of {} and {} is: {}".format(a, b, sum_result))
-print("The difference between {} and {} is: {}".format(a, b, diff_result))
-print("The product of {} and {} is: {}".format(a, b, prod_result))
-print("The quotient of {} divided by {} is: {}".format(a, b, quot_result))
+
+if __name__ = "__main__":
+    print("The sum of {} and {} is: {}".format(a, b, sum_result))
+    print("The difference between {} and {} is: {}".format(a, b, diff_result))
+    print("The product of {} and {} is: {}".format(a, b, prod_result))
+    print("The quotient of {} divided by {} is: {}".format(a, b, quot_result))
