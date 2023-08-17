cf38e0e858089e6acb1d8129ab8189f24c745dd4
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 17:08:31 +0300

    A program that imports a function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_o import add
+from add_0 import add
+
 a = 1
 b = 2
-if __name == "__main__":
-    print('{} + {} = {}'.format(a, b, add(a, b)
+
+if __name__ == "__main__":
+    print('{} + {} = {}'.format(a, b, add(a, b)))
