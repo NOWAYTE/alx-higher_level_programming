6e140f285af538a70d0c9c6c7a96b96abe90fdc3
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 19:06:18 +0300

    Fix compilation error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     argv = sys.argv
 
     for i in range(len(argv) - 1):
-        result = arg[1] + argv[2]
+        result += arg[i]
 
         print('{}'.format(result))
