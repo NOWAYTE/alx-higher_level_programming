d081e6a7e035ec5901262601e553c935ea45f661
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 18:54:58 +0300

    Code update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,14 @@ from sys import argv
 if __name__ == "__main__":
     n = len(argv)
 
+    if n == 0:
+        print("0 arguments")
+
+    elif n == 1:
+        print("1 arguments")
+
+    else:
+        print('{} arguments:'.format(n))
+
     for i in range(n):
         print('{}: {}'.format(i, argv[1]))
