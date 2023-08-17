190ddb956f839c7d4c68e1d95629a5f4b9818e3f
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 19:10:26 +0300

    Fix code errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     argv = sys.argv
 
     for i in range(len(argv) - 1):
-        result += int(arg[i] + 1)
+        result += arg[i + 1]
 
         print('{}'.format(result))
