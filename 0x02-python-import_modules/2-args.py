dee902178f226743a914deee1aa1f6aa91a7391c
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 18:38:16 +0300

    Fix code error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if  __name__ = "__main__":
-    import sys
-    count = len(sys.argv) - 1
+import sys
+count = len(sys.argv) - 1
+if __name__ == "__main__":
     for i in range(count):
-    print("{}: arguments {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: arguments {}".format(i + 1, sys.argv[i + 1]))
