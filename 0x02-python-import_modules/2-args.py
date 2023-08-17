050e38e6fe73140eb3a6c1219c9161ed2c1761d0
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-17 18:41:36 +0300

    Code update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     for i in range(count):
-        print("{}: arguments {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
