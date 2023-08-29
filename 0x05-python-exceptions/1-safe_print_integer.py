f157b39e8a506c8cdf525df320d9cbad2a7982b3
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-29 03:53:52 +0300

    Code update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = '{:d}'.formtat(value)
-        print(value)
+        v = '{:d}'.formtat(value)
+        print(v)
         return True
     except (TypeError, ValueError):
         return False
