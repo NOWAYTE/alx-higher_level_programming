3f9aec2cc2918cf7472db5d6f94bbe92a10f5631
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-29 04:20:48 +0300

    Fix code errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print('{}'.format(my_list[i]), end="")
+            print('{:d}'.format(my_list[i]), end="")
             count += 1
 
     except (ValueError, TypeError, IndexError):
         pass
     print()
-
     return count
-
