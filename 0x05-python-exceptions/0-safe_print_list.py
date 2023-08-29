66a14f82021e4788d3cd3ac84f3af6d4d43aee5c
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-29 03:23:30 +0300

    Code update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-try:
-    def safe_print_list(my_list=[], x=0):
+
+def safe_print_list(my_list[], x=0):
+    try:
         for i in range(x + 1):
-            print(my_list[x])
-except Exception as a:
-    print(a)
+            print(my_list[i])
+    except Exception as a:
+        print(a)
