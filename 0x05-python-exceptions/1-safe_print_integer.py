4133107ee00d01153c1aac4bcaf1714edca8d827
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-29 03:56:46 +0300

    Fix typo AttributeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        v = '{:d}'.formtat(value)
+        v = '{:d}'.format(value)
         print(v)
         return True
     except (TypeError, ValueError):
