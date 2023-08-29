bcc22f27480159009fd664a12eb741ac1f003c13
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-29 05:55:10 +0300

    Add else statement

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,9 +4,8 @@ def safe_print_integer_err(value):
     try:
         if isinstance(value, int):
             print('{:d}'.format(value))
-            return True
-        else:
-            raise TypeError("Value not integer")
     except Exception as a:
-        sys.stderr.write('Exception: {}'.format(str(a)))
+        sys.stderr.write('Exception: {}\n'.format(str(a)))
         return False
+    else:
+        return True
