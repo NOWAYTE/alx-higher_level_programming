8f1eff169b1f01d40059e266b5efe26831d5f489
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-30 03:14:10 +0300

    A class tha defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+
+
+class Square:
+    """A class that defines a square and a private instance attribute size"""
+    def __inti__(self, size=0):
+        self.__size = size
+        try:
+            if isinstance(size, int):
+                pass
+            if size < 0:
+                pass
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
