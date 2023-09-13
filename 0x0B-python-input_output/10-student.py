70318b0f7a0910c24f5742f0f4a8ec71d3dc3423
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-13 05:41:02 +0300

    A class Student that defines a method that retrieves a dictionary representation of a Student instance

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and all(isinstance(attr, str) for attr in attrs)):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-Student = __import__('9-student').Student
+Student = __import__('10-student').Student
 
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
+
+guillaume@ubuntu:~/0x0B$ ./10-main.py 
+{'age': 23, 'last_name': 'Doe', 'first_name': 'John'}
+{'age': 27, 'first_name': 'Bob'}
+{'age': 27}
