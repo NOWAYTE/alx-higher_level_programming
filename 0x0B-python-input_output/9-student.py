b7949c5b22cdbf840751a027fba3c6f5659f5513
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-13 05:27:48 +0300

    Fix code error

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,11 +2,11 @@
 """A student Class"""
 
 
-Class Student:
+class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            return self.__dict__
+    def to_json(self):
+        return self.__dict__
