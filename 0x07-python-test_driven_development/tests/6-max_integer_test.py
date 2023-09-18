220e344a15e89c708a365e9846f2fd62f8dd39ef
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-18 23:08:37 +0300

    Unittest for function that find max integer in a list of integers

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for max_integer()"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unitttest.TestCase):
+    """unittest for max_integer"""
+
+
+    def test_l(self):
+        list_ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(list_ordered), 4)
+
+    def test_u(self):
+        list_unordered = [1, 2, 4, 3]
+        self.assertEqual(max_integer(list_unorded)
+
+    def test_e(self):
+        list_empty = []
+        self.assertEqual(max_integer(empty), None)
+
+
+    if __name__ = '__main__':
+        unittest.main
