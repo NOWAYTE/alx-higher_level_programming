68e8f0fa270e0622abf2fdb9b928b0c9366e5ed9
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-18 14:08:37 +0300

    Test case update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""A function that prints a name """
+"""A function that prints a name"""
 
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
+
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
+
         raise TypeError("last_name must be a string")
 
-
-    return f"My name is {first_name} {last_name}"
+    print("My name is {} {}".format(first_name, last_name))
