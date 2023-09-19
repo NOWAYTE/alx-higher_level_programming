a6231d8d446bd83e8b0050418c803bfe429386e4
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-09-19 15:22:36 +0300

    Update

---
+++ unitTest/README.md
@@ -0,0 +1,4 @@
+doctest through documentation
+Purpose: Write automated tests as part of the documentation for a module 
+
+
---
+++ unitTest/unit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import unittest
+
+class simpleTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
