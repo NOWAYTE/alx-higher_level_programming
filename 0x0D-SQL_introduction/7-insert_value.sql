a43d8bbf165c3bfc4b00ce306d21a87efb841996
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-01 01:17:29 +0300

    Update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- a script that inserts a new row on the table
-INSERT INTO 'first_table('id', 'name') VALUES (89, 'Best School');
+INSERT INTO 'first_table ('id', 'name') VALUES (89, 'Best School');
