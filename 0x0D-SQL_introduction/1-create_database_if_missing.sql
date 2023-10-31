875fb1ed8ff3e565ce19458436b9eda751b59449
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-10-31 23:58:23 +0300

    Code update

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
--- A script that creates a database 
-
-CREATE DATABASE IF NOT EXIST hbtn_0c_0;
+-- A script that creates a database hbtn_0c_0 in MYSQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
