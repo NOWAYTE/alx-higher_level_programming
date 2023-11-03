20f4e2e76bcb5bfa3b76eea3c695f9b951b1e24e
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-03 18:24:06 +0300

    Code update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
 -- a script that creates user user_0d_1
 
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES
+ON *.* 
 
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
