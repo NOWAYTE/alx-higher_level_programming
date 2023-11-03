262809d2024fd2770c6e392c2caa4ecebe259e61
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-03 18:13:36 +0300

    fix code error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,6 @@
 -- a script that creates user user_0d_1
 
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES;
---ON *.*
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
---TO 'user_0d_1'@'localhost';
---FLUSH PRIVILEGES;
+FLUSH PRIVILEGES;
