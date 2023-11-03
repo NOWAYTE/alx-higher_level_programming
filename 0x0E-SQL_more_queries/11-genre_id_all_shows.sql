eec55b78f3b17ca87ce7fbd97df0d762875046d6
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-03 23:57:29 +0300

    Fix code error

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,7 @@
--- a script that lists all shows contained in the database
+-- a scrip that lists all shows contained in the datase htbn_0d_tvshows
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_show_genres INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+  FROM tv_show_genres
+  INNER JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id;
