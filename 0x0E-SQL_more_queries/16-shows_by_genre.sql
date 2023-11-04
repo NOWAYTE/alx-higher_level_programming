f5fb82f60af5046612d5fc7fb1e321830e57c45a
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-04 08:25:52 +0300

    A script that lists all shows and all genres linked to that show from the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- a scrip that lists all shows and all genres linked to a show
+
+SELECT tv_shows.title, tv_genres.name
+  FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genre   ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_genres.name;
