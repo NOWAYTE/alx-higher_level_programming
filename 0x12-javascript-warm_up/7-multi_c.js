d0e0df47571337fd8edf919dc0d1484cd6b84d08
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-20 18:07:27 +0300

    Code update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 
 if (Number.isInteger(Number(args[0]))) {
-  for (let i = 0; i <= args[0]; i++) {
+  for (let i = 0; i < args[0]; i++) {
     console.log('C is fun');
   }
 } else {
