7007fdfc95b90240f9f1f7e8969243b6fd188738
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-21 11:30:50 +0300

    semistandard --fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,13 +2,13 @@
 
 const args = process.argv.slice(2);
 
-let num1 = parseInt(args[0]);
-let num2 = parseInt(args[1]);
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
 
-function add(a, b){
-	return a + b;
+function add (a, b) {
+  return a + b;
 }
 
-sum = add(num1, num2);
+const sum = add(num1, num2);
 
 console.log(`${sum}`);
