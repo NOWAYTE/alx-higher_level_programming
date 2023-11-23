9ef6704ec06f940ca06671a0f43249bcf2bca273
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-23 06:38:46 -0500

    a script that computes and printsa factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,17 +2,16 @@
 
 const args = process.argv.slice(2);
 
-function factorial(n){
-	if (isNaN(n) || n < 0){
-		return 1;
-	}
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
 
-	if (n === 0){
-		return 1;
-	}
+  if (n === 0) {
+    return 1;
+  }
 
-
-	return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 const input = args[0];
