63d9fa17eaecadcc8a063be93320ddc96987943a
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-12-01 05:21:05 +0300

    Cod e update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL,and displays the body of the response
+# A bash script that rakes in a URL and diplays the body of the response
 curl -sL "$1"
