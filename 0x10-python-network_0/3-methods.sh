9e85fbf6ea307866e83620778e9ab746cc660989
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-12-01 05:58:40 +0300

    Code update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# A bash script that takes in URL and displays all the HTTPS methods the server will accept
-curl -sI "$1" | grep  "allow" | cut -d "" -f 2-
+# takes in a URL and displays all HTTP methods the server will accept
+
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
