1fb2a87ca4d157a831711f36b47f814823a85958
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-12-01 05:01:08 +0300

    Code update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script that takes URL, and sends a request to that URL
-
-curl -s -w "$1" | wc -c
+#takes in a URL, sends a request to that URL,and displays the size of the body of the response
+curl -s "$1" | wc -c
