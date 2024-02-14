11be18c7151c95e3ed3d55a7010b6bcc6621b464
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-14 04:22:44 -0500

    Updatr

--- 0x0A-python-inheritanc/0-lookup.py
+++ 0x0A-python-inheritanc/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" A function that return the lists of available attributes and methods of an object """
+"""A function that return the lists of available attributes and methods of an object """
 
 
 def lookup(obj):
+   """Return a lists of objects attributes"""
    print(dir(obj))
