edf03c6bcbe175d5db5aa27069e54538491b3146
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-14 11:30:44 -0500

    A function that list available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Defines a function that lists attributes and methods"""
+
+
+def lookup(obj):
+    """A function that list attributes and methods of an object"""
+
+    return dir(obj)
