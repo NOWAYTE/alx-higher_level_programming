3f89d1dfaf25b06e57461de4498f2fecd821a245
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-20 14:20:32 -0500

    code update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
---A script that creates the MySQL SERVER user
-CREATE IF NOT EXIST user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 ON *.*
-TO 'user_0d_1';
-FLUSH PRIVILEGES;
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;:wq
