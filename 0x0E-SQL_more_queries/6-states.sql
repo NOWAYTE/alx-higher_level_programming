67d585cd36ae4e9287ac38a924cf06e51f870ccc
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-20 16:39:27 -0500

    Update

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id)
