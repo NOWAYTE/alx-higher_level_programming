df3a51c97c31201ffa4db37a968e5ad8871b4a78
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-20 17:22:10 -0500

    Update

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,4 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
