86cda581efc326525aacfbcb1c87de6b95dfe824
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-24 10:45:00 +0300

    Update

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,4 @@
 -- a script that lists all cities contained in the database htbn_0d_usa
-SELECT cities.id, cities.name, states.name FROM CITIES INNER JOIN ON cities.states_id = states.id;
+SELECT cities.id, cities.name, states.name
+FROM CITIES
+INNER JOIN ON cities.states_id = states.id;
