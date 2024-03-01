ff98f1a0a02a4bafa44f501f3dc3f47fd5124fdc
jelimo-charity <charityjelimo893@gmail.com>
2024-03-01 16:12:33 +0300

    A function that returns True if the object is an instanc of a class that inherited

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines an function that checks for an object"""
+
+
+def inherits_from(obj, a_class):
+    """Function that checks if an object is an instance
+      or an instance of inherited class
+
+    Args:
+        obj: Object to check
+        a_class: class to be compared with
+
+    Returns:
+        returns True is object is is an instance or an instance
+        of inherited class
+
+
+    """
+    
+    if isinstance(obj, a_class):
+        return True
+    return False
