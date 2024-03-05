55bda135e37f5787256951423066a549d2353972
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-05 12:46:27 +0300

    Code update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,7 +16,6 @@ def inherits_from(obj, a_class):
 
 
     """
-    
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
