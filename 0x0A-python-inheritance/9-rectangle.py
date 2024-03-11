71699072a5138c3cccb17a7212d7d98849a0c57b
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-11 11:04:25 +0300

    Code update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,11 +20,13 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Returns area of the rectangle."""
+
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        """Return the string representation of a Rectangle."""
+
+        """string = "[" + str(self.__class__.__name__) + "] """
+
+        return f"str(self.__width) + "/" + str(self.__height)"
