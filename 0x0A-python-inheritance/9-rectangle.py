8372427db25943ed2df32388da2ba7bbc004c20d
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-11 10:58:28 +0300

    Update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,7 +2,7 @@
 
 """Defines a Rectangle class """
 
-BaseGeometry = __import__('8-rectangle.py').BaeGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
