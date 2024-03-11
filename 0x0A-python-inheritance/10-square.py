94e58a9aeb4a27c2b615d1008e00c2fb339c57b4
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-11 12:10:53 +0300

    A square class that inherits from BaseGeometry

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Defines a Rectangle subclass Square."""
+"""Defines a Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -8,11 +8,13 @@ class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initialize a square.
 
         Args:
             size (int): The size of the new square.
         """
+
         self.integer_validator("size", size)
+
         super().__init__(size, size)
         self.__size = size
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Defines a Square class """
+
+
+class Square:
+    """Represents a Square class that inherits from BaseRectangle"""
+
+    def __init__(self, size):
+        """Instance of a square
+
+        Args:
+            size(int): Represents the size of the square
+
+        """
+
+        self.__size = size
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+
+        def area(self):
+            """Returns the area of the square"""
+
+            return self.__size * self.__size
+
+        def __str__(self):
+            """Returns the string representation of the square"""
+
+            return f"{[self.__class__.__name__}] {self.__width}/{self.__height}"
