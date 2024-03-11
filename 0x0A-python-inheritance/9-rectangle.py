f736cf9361d620772b5962b8db1a831d1d5a5571
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-11 10:59:37 +0300

    Update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 
-"""Defines a Rectangle class """
-
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle class that inherits from BaseGeometry"""
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """instaniate rectangle
+        """Intialize a new Rectangle.
 
         Args:
-            width(int): Width of the new rectangle
-            Height(int): Height of th new rectangle
-
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
 
-        def area(self):
-            """Defines the area of a rectangle"""
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
 
-            return self.__width * self.__heigth
-
-        def __str__(self):
-            """Returns string representation of the object"""
-
-            return f"[Rectangle] {self.__width}/self__height}"
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
---
+++ 0x0A-python-inheritance/hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Defines a Rectangle class """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """instaniate rectangle
+
+        Args:
+            width(int): Width of the new rectangle
+            Height(int): Height of th new rectangle
+
+        """
+
+        def area(self):
+            """Defines the area of a rectangle"""
+
+            return self.__width * self.__heigth
+
+        def __str__(self):
+            """Returns string representation of the object"""
+
+            return f"[Rectangle] {self.__width}/self__height}"
