a938a6365fb21a2c4e0ac389a5c8f87a5c10d15b
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-16 00:21:19 +0300

    Update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,13 @@
 const argv = process.argv.slice(2).map(arg => parseInt(arg));
 
 if (argv.length === 0 || argv.length === 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    const sorted = argv.sort((a, b) => b - a);
+  const sorted = argv.sort((a, b) => b - a);
 
-    if (sorted[1] === sorted[0]) {
-        console.log(sorted[2]);
-    } else {
-        console.log(sorted[1]);
-    }
+  if (sorted[1] === sorted[0]) {
+    console.log(sorted[2]);
+  } else {
+    console.log(sorted[1]);
+  }
 }
-
