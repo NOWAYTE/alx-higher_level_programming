a1ed7135473c96de806ad0187d71d88e84c036cf
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-15 21:07:33 +0300

    A script that prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+if (!isNaN(parseInt(argv[0]))) { for (let i = 0; i < argv[0]; i++) { console.log('C is fun'); } } else { console.log('Missing number of occurences'); }
