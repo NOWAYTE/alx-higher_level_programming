c8003dcef274f50c9fe62e18b2816a6919791051
danielkamaunganga3@gmail.com <danielkamaunganga3@gmail.com>
2024-03-16 13:06:32 +0300

    Code update

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-const argv = process.argv.slice(2);
-const x = parseInt(argv[0]);
-const y = parseInt(argv[1]);
-
-function add (x, y) {
-  return x + y;
-}
-
-console.log(add(x, y));
+const add = (a, b) => {
+  return a + b;
+};
