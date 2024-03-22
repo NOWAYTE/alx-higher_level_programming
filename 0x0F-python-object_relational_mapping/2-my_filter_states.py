cbc5b0fc2f3522e4294a4ffeb6f7fadb1ac3ace1
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-23 02:10:14 +0300

    A script that displays all values in the states table where the name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states tables
+where the name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    results = cur.fetchall()
+
+    for i in results:
+        if i[1] == sys.argv[4]:
+            print(i)
+
+    db.close()
