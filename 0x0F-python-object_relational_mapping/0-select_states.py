9bdb5b409d73f6435cf3e7fe203fb06a29d28cae
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-22 21:39:19 +0300

    Code update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N (upper N)
- from the database hbtn_0e_0_usa
- Usage: ./1-filter_states.py <mysql username>
-                            <mysql password>
-                            <database name>"""
+"""Lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+Usage: ./1-filter_states.py <mysql username>
+                           <mysql password>
+                           <database name>"""
 
 import sys
-from unicodedata import name
-import MySQLdb
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Define base class for declarative class definitions
+Base = declarative_base()
+
+# Define State class which represents the states table
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    # Connect to the MySQL database using SQLAlchemy
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the states starting with "N"
+    states_starting_with_n = session.query(State).filter(State.name.like('N%')).order_by(State.id).all()
+
+    # Print the results
+    for state in states_starting_with_n:
+        print(state.name)
+
