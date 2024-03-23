566830dd17de656fb37f04785a3e96bc641c3e1a
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-23 03:22:05 +0300

    Fix code error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" takes in an argument
-    and displays all values
-    in the states table of
-    hbtn_0e_0_usa where name matches the argument
-     Usage: ./2-my_filter_states.py <mysql username>
-                                    <mysql password>
-                                    <database name>
-                                    <state name searched>
-"""
+"""A script that takes in an argument and displays allment """
+
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
-    [print(state) for state in c.fetchall()]
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    results = cur.fetchall()
+
+    for i in results:
+        if i[1] == sys.argv[4]:
+            print(i)
+
+    db.close()
