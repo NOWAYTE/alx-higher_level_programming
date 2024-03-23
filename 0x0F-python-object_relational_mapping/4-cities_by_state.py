741802d338fe02904721d5bb0a901a651a96f59d
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-23 14:04:00 +0300

    Code update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,12 +13,11 @@ if __name__ == '__main__':
             db=sys.argv[3]
             )
     cur = db.cursor()
-    query = """
-            SELECT cities.id, cities.name, states.name
-            FROM cities
-            JOIN states 
-            ON cities.id  = states.id
-            ORDER BY cities.id ASC"""
+    query = """SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON cities.state_id = states.id
+                ORDER BY cities.id ASC"""
 
     cur.execute(query)
 
