397a61dcecae9e1f0c129dc8eba5aff1a4eb346f
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-23 13:08:52 +0300

    Safe version of 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A script that takes in an argument and displays all values in the states """
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    results = cur.fetchall()
+
+    for i in results:
+        if i[1] == sys.argv[4]:
+            print(i)
+
+    db.close()
