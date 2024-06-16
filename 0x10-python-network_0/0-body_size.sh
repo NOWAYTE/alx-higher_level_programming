4c73316709334f27437590b61ed706e875a3db74
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-06-16 18:42:03 +0300

    Reduce script to 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#script that takes in an URL and displys the size of the body
-if [ $# -eq 0 ]; then
-        echo "Usage: $0 URL"
-        exit
-fi
-
+#displayhs the size of the body response
 curl -sI "$1" | grep -i content-length | awk '{print $2}'
-
-exit 0
