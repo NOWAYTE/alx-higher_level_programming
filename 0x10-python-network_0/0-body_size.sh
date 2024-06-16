ebb3dc5c8250b0ae8960f1ea50437c9b10499911
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-06-16 18:23:25 +0300

    A bash script that takes a URL and diplays the body of the response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Script that takes in an URL and displys the size of the body
+
+if [ $# -eq 0 ]; then
+        echo "Usage: $0 URL"
+        exit
+fi
+
+curl -sI $1 | grep -i content-length | awk '{print $2}'
+
+exit 0
