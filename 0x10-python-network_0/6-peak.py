991de63dd3d69afa8b2f2b7bbf1c87006398f7f9
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-06-17 18:34:23 +0300

    Fix code error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-#function that finds a peak in a list unsorted integers
+"""
+function that finds the peak in an unsorted array
+
+"""
+
 
 def find_peak(list_of_integers):
+    """
+    returns peak in a unsorted array 
+
+    """
     if len(list_of_integers) == 0:
-        return None;
+        return None
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     if len(list_of_integers) == 2:
@@ -13,11 +21,12 @@ def find_peak(list_of_integers):
 
     peak = list_of_integers[m]
 
-    if peak > (list_of_integers - 1) and peak > (list_of_integers + 1):
+    if peak > list_of_integers[m - 1] and peak > list_of_integers[m + 1]:
         return peak
-    elif peak < list_of_integers - 1:
-        return find_peak(list_of_integers[:mid])
+
+    elif peak < list_of_integers[m - 1]:
+
+        return find_peak(list_of_integers[:m])
     else:
-        return find_peak(list_of_integers[mid + 1])
 
-
+        return find_peak(list_of_integers[m + 1:])
