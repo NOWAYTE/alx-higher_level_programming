308c53546104898c5101d762c2152e3dc0169d8b
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-06-17 17:28:34 +0300

    Fix typo

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that send a POST request and displays the body of the response 
-curl -s -X POST -d"email=test@gmail.com&subject=I Will always be here for PLD" "$1"
+curl -s -X POST -d"email=test@gmail.com&subject=I will always be here for PLD" "$1"
