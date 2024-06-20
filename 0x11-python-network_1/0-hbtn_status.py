a51fa369241ca07d86306d4601a164e66e720124
Danilkamaunganga <danielkamaunganga3@gmail.com>
2024-06-20 23:17:58 +0300

    Update formatting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         read = response.read()
         print("Body response: ")
-        print("\t-  type: {}".format(type(read)))
-        print("\t-  content: {}".format(read))
-        print("\t-  utf8 content: {}".format(read.decode(encoding='utf-8')))
+        print("\t- type: {}".format(type(read)))
+        print("\t- content: {}".format(read))
+        print("\t- utf8 content: {}".format(read.decode(encoding='utf-8')))
