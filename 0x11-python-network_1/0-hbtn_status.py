ddda209d070a3bf17c799fb27eb70069d839e8a3
Danilkamaunganga <danielkamaunganga3@gmail.com>
2024-06-20 23:20:55 +0300

    Code updae

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-A python script that fetches from a URL
-"""
-
-
+"""fetches https://intranet.hbtn.io/status"""
 from urllib import request
 
 if __name__ == "__main__":
-
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        read = response.read()
-        print("Body response: ")
-        print("\t- type: {}".format(type(read)))
-        print("\t- content: {}".format(read))
-        print("\t- utf8 content: {}".format(read.decode(encoding='utf-8')))
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
