dae87e1361f51baea833be03c746a2fade9d4f0a
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 18:29:21 +0300

    Change output format

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """A script that fetches https:alx-intranet.hbtn.io/status"""
 
-import sys
 import requests
 
 if __name__ == "__main__":
-    request = requests.get('https://alx-intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body Response:")
-    print("\t -type: {}".format(type(request.text)))
-    print("\t -contennt {}".format(request.text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- contennt {}".format(req.text))
