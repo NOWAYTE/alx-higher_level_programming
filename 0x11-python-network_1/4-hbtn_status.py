a8ba9d524476b740f739b0077974309f9dfb69d7
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 18:24:29 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A script that fetches https:alx-intranet.hbtn.io/status"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    request = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body Reponsae:")
+    print("\t -type: {}".format(type(request.text)))
+    print("\t -contennt {}".format(request.text))
