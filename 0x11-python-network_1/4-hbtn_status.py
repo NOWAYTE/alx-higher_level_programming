aeb20b176a070c994232b7a36beba7c4b72bf488
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 18:43:05 +0300

    Update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""A script that fetches https:alx-intranet.hbtn.io/status"""
+"""
+ fetches https://alx-intranet.hbtn.io/status
+"""
 
 import requests
 
-if __name__ == "__main__":
-    req = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body Response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
