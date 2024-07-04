a588fbe64fd8113a42ac13bf36041e6fbbaf5f35
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 22:43:12 +0300

    Fix pycodestyle error

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == '__main__':
     url = sys.argv[1]
     par = {'email': sys.argv[2]}
-    res = requests.post(url,data=par)
+    res = requests.post(url, data=par)
     print("{}".format(res.text))
