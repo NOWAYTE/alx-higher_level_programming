598d89ea312c7763541208292a44cfbb695e82fd
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 18:02:29 +0300

    CFetch correct argument

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,10 +7,10 @@ from urllib.error import URLError, HTTPError
 
 if __name__ == "__main__":
     if (sys.argv[1]):
-        request = urllib.request.Request(sys.argv[0])
+        request = urllib.request.Request(sys.argv[1])
 
     try:
         with urllib.request.urlopen(request) as response:
-            print("{}".format(reponse.read()))
+            print("{}".format(response.read().decode("utf-8")))
     except urllib.error.HTTPError as e:
         print("{}".format(e.code))
