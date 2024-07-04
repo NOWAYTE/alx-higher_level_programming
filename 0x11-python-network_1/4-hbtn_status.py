b831b62d288c9f423e3941fe6c837fc49043265e
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-04 18:39:01 +0300

    Fix code error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body Response:")
     print("\t- type: {}".format(type(req.text)))
-    print("\t- contennt: {}".format(req.text))
+    print("\t- content: {}".format(req.text))
