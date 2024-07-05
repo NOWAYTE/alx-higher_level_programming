a3bb7348c99c22e4cfe843e32f8dbd4d98e37b25
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-07-05 21:27:30 +0300

    Script that uses basic authentication with personal access token as password

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A python script that takes in github credentials and 
+uses the Github API to display your id 
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    url = " https://api.github.com/user"
+
+    value = {"user":  user,
+            "passwd": passwd}
+
+    try:
+        response = requests.get(url, auth=(user, passwd))
+
+        json = response.json()
+
+        print("{}".format(json.get("id")))
+    
+    except ValueError:
+        print("Hello")
