37b7deeaa0d239690e0680f1e1265e541541cf58
NOWAYTE <danielkamaunganga3@gmail.com>
2024-07-29 18:16:57 +0300

    Add doscstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Contains a base class"""
 
 class Base:
     """Base Class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initializes an instance"""
         if id is not None:
             self.id = id
         else:
