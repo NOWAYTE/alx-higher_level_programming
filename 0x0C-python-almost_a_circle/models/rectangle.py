41d5a3ba0a8398836a3b20bc9260004f5b90cd03
NOWAYTE <danielkamaunganga3@gmail.com>
2024-07-30 14:45:54 +0300

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class"""
 
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -79,7 +79,8 @@ class Rectangle(Base):
         """Prits rectangle instance with #"""
 
         rectangle = ""
-        for i in range(self.y):
-            rectangle += "#" * self.x + "\n"
+
+        for i in range(self.height):
+            rectangle += "#" * self.width + "\n"
 
         print(rectangle)
