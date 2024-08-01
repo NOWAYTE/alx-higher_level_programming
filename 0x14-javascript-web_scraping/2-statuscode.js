09115e1185e8a328385b0f0b07d6867eb2c965d3
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-01 03:48:24 +0300

    Update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-    if (error) throw error;
+  if (error) throw error;
 
-    console.log("code:", response.statusCode);
+  console.log('code:', response.statusCode);
 });
-
